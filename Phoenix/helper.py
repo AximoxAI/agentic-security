2bd731d7e844203f924f84726140712cc3390d1e
Priyanka <priyanka@ee.iitr.ac.in>
2025-07-08 19:54:51 +0530

    evalauted router and tools

--- Phoenix/helper.py
+++ Phoenix/helper.py
@@ -1,17 +1,28 @@
-
 import os
 from dotenv import load_dotenv, find_dotenv
-                     
+import phoenix as px
+
 def load_env():
-    _ = load_dotenv(find_dotenv(), override=True)
+    load_dotenv(find_dotenv(), override=True)
 
 def get_openai_api_key():
     load_env()
     openai_api_key = os.getenv("OPENAI_API_KEY")
     return openai_api_key
 
-
 def get_phoenix_endpoint():
+    """Returns the Phoenix UI endpoint - no need to set PHOENIX_COLLECTOR_ENDPOINT"""
+    return "http://localhost:6006/"
+
+def setup_phoenix():
+    """Properly sets up Phoenix without collector endpoint issues"""
     load_env()
-    phoenix_endpoint = os.getenv("PHOENIX_COLLECTOR_ENDPOINT")
-    return phoenix_endpoint
+    
+    # Make sure no collector endpoint is set (this was causing your issue)
+    if 'PHOENIX_COLLECTOR_ENDPOINT' in os.environ:
+        del os.environ['PHOENIX_COLLECTOR_ENDPOINT']
+    
+    # Launch Phoenix app
+    session = px.launch_app(run_in_thread=True)
+    print(f"Phoenix is running at: {get_phoenix_endpoint()}")
+    return session
